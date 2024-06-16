59916546f28c69273a04795e592b581aace665db
paris26 <paris.gvr@gmail.com>
2024-06-16 13:26:59 +0300

    Update at 2 hours

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
 import Header from './components/Header'
-
+import Hero from './components/Hero'
+import Services from './components/Services'
 
 const App = () => {
 
@@ -9,6 +12,10 @@ const App = () => {
     
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
 
       <ButtonGradient />
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,7 @@ const Button = ({ className, href, onClick, children , px, white}) => {
 
   const renderButton = () => {
     return (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
         <span className={spanClasses}>{children}</span>
         {ButtonSvg(white)}
     </ button>
